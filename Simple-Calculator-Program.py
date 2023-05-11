6e0c99ab39593778d636eaa4febf0acec0b4eb7c
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-05-11 19:38:45 +0800

    insertion of except statement

--- Simple-Calculator-Program.py
+++ Simple-Calculator-Program.py
@@ -53,7 +53,9 @@ def division_num():
         result = float(num1.get()) / float(num2.get())
         result = round(result, 4)
         label4.config(text = result)
-    
+    except:
+        messagebox.showerror('Error detected', 'Error: Invalid input. Please enter a valid input')
+        
 # define function for clear input
 
 # define function for reiteration
