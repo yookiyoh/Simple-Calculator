42af8429c5b173d828547a0c1945ebcdcaf82b83
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-05-11 18:58:05 +0800

    general instructions compliance

--- Simple-Calculator-Program.py
+++ Simple-Calculator-Program.py
@@ -2,3 +2,13 @@
 # BSCpE 1-5
 # Object-Oriented Programming | Assignment 5
 # Simple Calculator Program
+
+# General Instructions
+
+# 1: The application will ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication, and Division)
+# 2: The application will ask the user for two numbers
+# 3: Display the result
+# 4: The application will ask is the user wants to try again or not
+# 5: If yes, repeat Step 1
+# 6: If no, display "Thank you!" and the program will exit
+# 7: Use Python Function and appropriate Exceptions to capture errors during runtime
